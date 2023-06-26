56c90a84c89bb80666f0c426a37f16d1d2969863
Abdur-Rahim-Tipu <biplobh412@gmail.com>
2023-06-26 16:04:17 +0600

    first commit

---
+++ CandidateBlazorApp/CandidateBlazorApp/Shared/ViewModels/FileUploadResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidateBlazorApp.Shared.ViewModels
+{
+    public class FileUploadResponse
+    {
+        public string UploadFile { get; set; } = default!;
+        public string UploadFileName { get; set; } = default!;
+    }
+}
